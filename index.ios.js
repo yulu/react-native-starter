070891fc917cb295e5088d5bbe4deae9c206d11c
Yu Lu <yulu8798@gmail.com>
2016-03-11 08:15:59 +0800

    modify with real data

--- index.ios.js
+++ index.ios.js
@@ -11,29 +11,79 @@ import React, {
     View,
 } from 'react-native';
 
-var MOCKED_MOVIES_DATA = [
-    {title: 'Title', year: '2015', posters: {thumnail: 'http://i.imgur.com/UePbdph.jpg'}},
-];
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
 
 class AwesomeProject extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            movies: null,
+        };
+    }
+    componentDidMount() {
+        this.fetchData();
+    }
+    fetchData() {
+        fetch(REQUEST_URL)
+            .then((response) => response.json())
+            .then((responseData) => {
+                this.setState({
+                    movies: responseData.movies,
+                });
+            })
+            .done();
+    }
     render() {
-        var movie = MOCKED_MOVIES_DATA[0];
+        if (!this.state.movies) {
+            return this.renderLoadingView();
+        }
+        
+        var movie = this.state.movies[0];
+        return this.renderMovie(movie);
+    }
+    renderLoadingView() {
         return (
             <View style={styles.container}>
-                <Text>{movie.title}</Text>
-                <Text>{movie.year}</Text>
-                <Image source={{uri: movie.posters.thumnail}} style={styles.thumnail} />
+                <Text>
+                    Loading movies...
+                </Text>
             </View>
         );
+    }
+    renderMovie(movie) {
+         return (
+            <View style={styles.container}>
+                <Image 
+                    source={{uri: movie.posters.thumnail}} 
+                    style={styles.thumnail} 
+                />
+                <View style={styles.rightContainer}>
+                    <Text style={styles.title}>{movie.title}</Text>
+                    <Text style={styles.year}>{movie.year}</Text>
+                </View>
+            </View>
+        );       
     }
 }
     
 var styles = StyleSheet.create({
     container: {
         flex: 1,
+        flexDirection: 'row',
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#F5FCFF',
+    },
+    rightContainer: {
+        flex: 1,
+    },
+    title: {
+        fontSize: 20,
+        marginBottom: 8,
+        textAlign: 'center',
+    },
+    year: {
+        textAlign: 'center',
     },
     thumnail: {
         width: 53,
